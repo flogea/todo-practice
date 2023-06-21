6ddd88033f5f863321f6b9a90cf7cbfbdf90ba3c
flogea <katua.logvinova@gmail.com>
2023-06-21 16:47:53 +0300

    webpack config with methods

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const BuildConfig = {
-  mode: 'development',
-  // isDev,
-  port: env.PORT || 3000,
   path: {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     dist: path.resolve(__dirname, 'dist'),
@@ -15,75 +12,53 @@ const BuildConfig = {
   },
 };
 
+// const resolvePath = (dir) => {
+//   return path.join(__dirname, '..', dir);
+// };
+
+const getRules = () => {
+  return [
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(css|scss)$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+    {
+      test: /\.(png|jpg|gif|jpeg)$/,
+      use: ['file-loader'],
+    },
+    {
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    },
+  ];
+};
+
+const getPlugins = () => {
+  return [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: BuildConfig.path.html,
+    }),
+  ];
+};
+
 module.exports = {
-  mode: BuildConfig.mode,
-  entry: ['@babel/polyfill', BuildConfig.path.entry],
+  entry: BuildConfig.path.entry,
   devtool: 'inline-source-map',
   output: {
     path: BuildConfig.path.dist,
     filename: '[name].[hash].js',
   },
-  devServer: {
-    port: 3000,
-  },
   resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+    extensions: ['.ts', '.js', '.tsx'],
   },
-  plugins: [new HTMLWebpackPlugin({ template: BuildConfig.path.html }), new CleanWebpackPlugin()],
+  plugins: getPlugins(),
   module: {
-    rules: [
-      {
-        test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(jpg|jpeg|png)/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.m?jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.ts?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-          // options: {
-          //   presets: ['@babel/preset-typescript'],
-          // },
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-          // options: {
-          //   presets: ['@babel/preset-typescript'],
-          // },
-        },
-      },
-    ],
+    rules: getRules(),
   },
 };
