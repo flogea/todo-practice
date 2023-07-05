5edfb25c1cc71a594b159599426968fe8910b0bf
Katua <katua.logvinova@gmail.com>
2023-07-05 20:47:19 +0300

    notifications

--- src/widgets/Notification.tsx
+++ src/widgets/Notification.tsx
@@ -4,7 +4,20 @@ import NotificationContext from '../context/NotificationContext';
 function Notification() {
   const { showNotification, type, message } = useContext(NotificationContext);
 
-  return showNotification && <div>{message}</div>;
+  const onHoverHandler = () => {};
+
+  return (
+    <div
+      className={`notification ${showNotification ? 'active' : ''}`}
+      onMouseEnter={onHoverHandler}>
+      <div className={`header ${type}`}>
+        <h3 className="type">{type}</h3>
+        <button className="closeBtn">&#10006;</button>
+      </div>
+      <div className="mainInfo">{message}</div>
+      <div className="timeline"></div>
+    </div>
+  );
 }
 
 export default Notification;
